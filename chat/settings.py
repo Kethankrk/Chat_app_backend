162f8ee29f2e6080e97a87fd7e3d912aa65e084b
Kethankrk <kethankrk@gmail.com>
2024-04-24 16:33:33 +0530

    .

--- chat/settings.py
+++ chat/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-7t*zt2hoeo7gc7hkyze0n-%!-e8++9-p=uwig6x_nhgoebq^6y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
